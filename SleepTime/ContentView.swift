1a75ff3972dbf5524e09d5a2cc93824c3e48d2ae
SangWoo <semo75@gmail.com>
2022-02-10 09:41:29 +0900

    Initial Commit

---
+++ SleepTime/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SleepTime
+//
+//  Created by SangWoo on 2022/02/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SleepTime/SleepTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepTimeApp.swift
+//  SleepTime
+//
+//  Created by SangWoo on 2022/02/10.
+//
+
+import SwiftUI
+
+@main
+struct SleepTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
